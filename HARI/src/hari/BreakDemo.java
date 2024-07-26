f142dc363f3915f88f3b113b288968080239f3d3
ELCOT <ELCOT@Lenovo>
2024-07-26 06:54:41 -0400

    for

--- HARI/src/hari/BreakDemo.java
+++ HARI/src/hari/BreakDemo.java
@@ -4,7 +4,7 @@ public class BreakDemo {
 
 	public static void main(String args[]) {
 		
-			for (int i=5;i<10; i++) {
+			for (int i=5;i<10;i++) {
 			if (i==5)
 		     break;
 			System.out.println(i);
