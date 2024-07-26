c6e036fa96fa9855eda0d3a28914190fbf847580
ELCOT <ELCOT@Lenovo>
2024-07-26 06:21:24 -0400

    hii

---
+++ HARI/src/hari/PrimitiveDatatype.java
@@ -0,0 +1,22 @@
+package hari;
+
+public class PrimitiveDatatype {
+
+	public static void main(String[] args) {
+		// widening / implicit type casting
+			byte b = 10;
+			int i = b;
+			System.out.println(i);
+			float f = 22.14f;
+			double d = f;
+			System.out.println(d);
+			char ch = 'A';
+			int i3 = ch;
+			System.out.println(i3);
+			char var1 = '\u00A7';
+			int i4 = var1;
+			System.out.println(i4);
+
+	}
+
+}
